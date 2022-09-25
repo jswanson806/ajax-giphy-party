6fc3475ddc9338b2683a3ccc10c7079e49278f95
jswanson806@icloud.com <jake@DESKTOP-0S9V7C7.localdomain>
2022-09-25 06:32:32 -0500

    finished exercise

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-// 1. Allow the user to search for a GIF and when the form is submitted, make an AJAX request to the Giphy API and return a single GIF
-// 2. Once the Giphy API has responded with data, append the GIF to the page
-// 3. Allow the user to search for as many GIFs as they would like and keep appending them to the page
-// 4. Allow the user to remove all of the GIFs by clicking a button
 
-async function findGif(term){
-    let res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${term}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
-    const num = Math.floor(Math.random() * 10);
-   
-    const gifImg = document.createElement('img');
-    gifImg.setAttribute('id', 'gifImg');
-    gifImg.setAttribute('src', res.data.data[num].url);
-    $("#gif-display").append(gifImg);
-    console.log(res.data.data[num].url);
+
+//await response from api.giphy containing matches for user search term
+async function findGif(term) {
+  let res = await axios.get(
+    `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=EHBAMezpffAObSISZXnTSA7bvHsf2tPQ`
+  );
+  
+  //generate random whole number between 0 and 10 and save to variable
+  const num = Math.floor(Math.random() * 20);
+
+  //save imgSrc to var using random num var as [i] to get a single image from res
+  const imgSrc = res.data.data[num].images.original.url;
+
+  //create new gifImg, set id attribute and src attribute to imgSrc var
+  const gifImg = $(`<img id='gif-img' src=${imgSrc}></img>`);
+
+  //append the image to the div container
+  $("#gif-display").append(gifImg);
+
+  //log the response from axios.get in the console
+  console.log(res);
 }
 
-$("#submit-btn").on('click', function (evt) {
-    evt.preventDefault();
-    const term = $("#search-input").val();
-    findGif(term);
+//on button click, grab value of input and pass to findGif()
+$("#submit-btn").on("click", function () {
+  const term = $("#search-input").val();
+  findGif(term);
 });
+
+//on reset button click, call removeAll()
+$("#reset-btn").on("click", function () {
+  removeAll();
+});
+
+//remove all children of the gif-display div, in this case, all gifImg elements
+function removeAll(){
+  $('#gif-display').children().remove();
+}
